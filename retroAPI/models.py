5d14bdab86a10945e1b6cceb6ae7152b0727201b
Terell Pigram <terell.pigram@gmail.com>
2017-08-13 23:50:58 -0400

    Returning title on Retro and RetroItem object

--- retroAPI/models.py
+++ retroAPI/models.py
@@ -9,6 +9,10 @@ class Team(models.Model):
 class Retro(models.Model):
     title = models.CharField(max_length=100)
     team = models.ForeignKey(Team, on_delete=models.CASCADE, null=True)
+    def __str__(self):
+        return self.title
 
 class RetroItem(models.Model):
     title = models.CharField(max_length=100)
+    def __str__(arg):
+        return self.title
